874f7dec4d931be4b47cd5d0ab751e4d074c2950
roloa <marshkip@gmail.com>
2019-08-14 19:16:07 +0900

    背面カメラバージョン２

--- fortnite/app.js
+++ fortnite/app.js
@@ -1,6 +1,25 @@
 
 isFirstStart = true;
 
+function startVideo2(){
+    var video = document.getElementById('videocamera');
+    var constraints = {
+        audio: false,
+        video: {
+            // スマホのバックカメラを使用
+            facingMode: 'environment'
+        }
+    };
+    //  カメラの映像を取得
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then((stream) => {
+            video.srcObject = stream;
+        })
+        .catch((err) => {
+            window.alert(err.name + ': ' + err.message);
+        });
+}
+
 function startVideo() {
 
   if( isFirstStart ){
