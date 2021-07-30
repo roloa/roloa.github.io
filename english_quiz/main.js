6cc45680e6eaa505f1c34ac392230ca886e109fa
Yut40 <marshkip@gmail.com>
2021-07-31 02:43:42 +0900

    hardmode

--- english_quiz/main.js
+++ english_quiz/main.js
@@ -15,6 +15,17 @@
       .then(response => response.json())
       .then(function(data){ quiz_dict = data; console.log('loaded!') } );
 
+    check_similar_word = function( word1, word2 ){
+        if( Math.random() < 0.0001 ){
+            // 確率で無条件
+            return true
+        }
+        if( word1.charAt(0) == word2.charAt(0) ){
+            return true
+        }
+        return false
+    }
+
     document.getElementById('button_quiz').onclick = function(){
         // ボタンが押された時のイベント
 
@@ -22,23 +33,76 @@
         answer_display_list[1].style["background-color"] = null
         answer_display_list[2].style["background-color"] = null
         answer_display_list[3].style["background-color"] = null
+        document.getElementById('quiz_description').innerHTML = ''
 
-
-        correct_answer = quiz_dict[Math.floor( Math.random() * 100 )]
+        correct_answer = quiz_dict[Math.floor( Math.random() * quiz_dict.length )]
 
         document.getElementById('quiz_en').innerHTML = correct_answer['en']
 
-        answer_display_list[0].innerHTML = quiz_dict[Math.floor( Math.random() * 100 )]['jp']
-        answer_display_list[1].innerHTML = quiz_dict[Math.floor( Math.random() * 100 )]['jp']
-        answer_display_list[2].innerHTML = quiz_dict[Math.floor( Math.random() * 100 )]['jp']
-        answer_display_list[3].innerHTML = quiz_dict[Math.floor( Math.random() * 100 )]['jp']
+        if( true ){
+            console.log(correct_answer)
 
+            var new_answer = null
+            while(true){
+                new_answer = quiz_dict[Math.floor( Math.random() * quiz_dict.length )]
+                if(check_similar_word( new_answer['en'], correct_answer['en'] )){
+                    break;
+                }
+            }
+            answer_display_list[0].innerHTML = new_answer['jp']
+            console.log(new_answer)
+            while(true){
+                new_answer = quiz_dict[Math.floor( Math.random() * quiz_dict.length )]
+                if( new_answer['jp'] == answer_display_list[0].innerHTML ){
+                    break;
+                }
+                if(check_similar_word( new_answer['en'], correct_answer['en'] )){
+                    break;
+                }
+            }
+            answer_display_list[1].innerHTML = new_answer['jp']
+            console.log(new_answer)
+            while(true){
+                new_answer = quiz_dict[Math.floor( Math.random() * quiz_dict.length )]
+                if( new_answer['jp'] == answer_display_list[0].innerHTML ||
+                    new_answer['jp'] == answer_display_list[1].innerHTML ){
+                    break;
+                }
+                if(check_similar_word( new_answer['en'], correct_answer['en'] )){
+                    break;
+                }
+            }
+            answer_display_list[2].innerHTML = new_answer['jp']
+            console.log(new_answer)
+            while(true){
+                new_answer = quiz_dict[Math.floor( Math.random() * quiz_dict.length )]
+                if( new_answer['jp'] == answer_display_list[0].innerHTML ||
+                    new_answer['jp'] == answer_display_list[1].innerHTML ||
+                    new_answer['jp'] == answer_display_list[2].innerHTML
+                ){
+                    break;
+                }
+                if(check_similar_word( new_answer['en'], correct_answer['en'] )){
+                    break;
+                }
+            }
+            answer_display_list[3].innerHTML = new_answer['jp']
+            console.log(new_answer)
+
+        } else {
+            answer_display_list[0].innerHTML = quiz_dict[Math.floor( Math.random() * quiz_dict.length )]['jp']
+            answer_display_list[1].innerHTML = quiz_dict[Math.floor( Math.random() * quiz_dict.length )]['jp']
+            answer_display_list[2].innerHTML = quiz_dict[Math.floor( Math.random() * quiz_dict.length )]['jp']
+            answer_display_list[3].innerHTML = quiz_dict[Math.floor( Math.random() * quiz_dict.length )]['jp']
+        }
         var correct_answer_index = Math.floor(Math.random() * 4);
         answer_display_list[correct_answer_index].innerHTML = correct_answer['jp']
 
     var on_answer_button = function( answer_numbers ){
         answer_display_list[answer_numbers].style["background-color"] = '#FCC'
         answer_display_list[correct_answer_index].style["background-color"] = '#CFC'
+        document.getElementById('quiz_description').innerHTML = correct_answer['description']
+
     }
 
     document.getElementById('answer1').onclick = function(){
