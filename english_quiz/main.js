73fdadf30687c3eb74bedd9f608c799dad3ffe06
Yut40 <marshkip@gmail.com>
2021-07-31 01:50:21 +0900

    answer color

--- english_quiz/main.js
+++ english_quiz/main.js
@@ -8,6 +8,8 @@
     answer_display_list[2] = document.getElementById('answer3')
     answer_display_list[3] = document.getElementById('answer4')
 
+    var correct_answer_index = 0
+
     // 辞書ファイルの読み込み
     fetch('https://roloa.github.io/english_quiz/mydata.json')
       .then(response => response.json())
@@ -15,6 +17,12 @@
 
     document.getElementById('button_quiz').onclick = function(){
         // ボタンが押された時のイベント
+
+        answer_display_list[0].style["background-color"] = null
+        answer_display_list[1].style["background-color"] = null
+        answer_display_list[2].style["background-color"] = null
+        answer_display_list[3].style["background-color"] = null
+
 
         correct_answer = quiz_dict[Math.floor( Math.random() * 100 )]
 
@@ -27,6 +35,26 @@
 
         var correct_answer_index = Math.floor(Math.random() * 4);
         answer_display_list[correct_answer_index].innerHTML = correct_answer['jp']
+
+    var on_answer_button = function( answer_numbers ){
+        answer_display_list[answer_numbers].style["background-color"] = '#FCC'
+        answer_display_list[correct_answer_index].style["background-color"] = '#CFC'
+    }
+
+    document.getElementById('answer1').onclick = function(){
+        on_answer_button(0)
+    }
+    document.getElementById('answer2').onclick = function(){
+        on_answer_button(1)
+    }
+    document.getElementById('answer3').onclick = function(){
+        on_answer_button(2)
+    }
+    document.getElementById('answer4').onclick = function(){
+        on_answer_button(3)
+    }
+
+
 
 }
 })();
