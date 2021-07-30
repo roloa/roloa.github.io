0b91792e16ea43da94504f17a5e0ae4e800bb81f
Yut40 <marshkip@gmail.com>
2021-07-31 05:24:20 +0900

    more ambiguous

--- english_quiz/main.js
+++ english_quiz/main.js
@@ -68,26 +68,23 @@
     document.getElementById('difficulty_normal').onclick()
 
     check_similar_word = function( word1, word2 ){
-        if( Math.random() < 0.00001 ){
+        if( Math.random() < 0.000001 ){
             // 確率で無条件
             return true
         }
-        if( word1.substr(-3) == word2.substr(-3) ){
-            return true
-        }
-        if( word1.substr(3) == word2.substr(3) ){
+        if( word1.substr(2) == word2.substr(2) ){
             return true
         }
         if( word1.substr(-2) == word2.substr(-2) ){
             return true
         }
-        if( Math.random() < 0.1 ){
+        if( Math.random() < 0.99 ){
             return false
         }
-        if( word1.substr(2) == word2.substr(2) ){
+        if( word1.substr(-1) == word2.substr(-1) ){
             return true
         }
-        if( Math.random() < 0.001 ){
+        if( Math.random() < 0.9999 ){
             return false
         }
         if( word1.charAt(0) == word2.charAt(0) ){
