d93d4d511288d3e75b61bf8d6f2232eb85d885f1
Yut40 <marshkip@gmail.com>
2021-07-31 01:31:56 +0900

    iikanji

--- english_quiz/main.js
+++ english_quiz/main.js
@@ -2,16 +2,31 @@
 
     var quiz_dict = {}
 
+    var answer_display_list = []
+    answer_display_list[0] = document.getElementById('answer1')
+    answer_display_list[1] = document.getElementById('answer2')
+    answer_display_list[2] = document.getElementById('answer3')
+    answer_display_list[3] = document.getElementById('answer4')
+
     // 辞書ファイルの読み込み
-    fetch('https://raw.githubusercontent.com/roloa/english_quiz/main/mydata.json')
+    fetch('https://roloa.github.io/english_quiz/mydata.json')
       .then(response => response.json())
       .then(function(data){ quiz_dict = data; console.log('loaded!') } );
 
     document.getElementById('button_quiz').onclick = function(){
         // ボタンが押された時のイベント
-        document.getElementById('en').innerHTML =
-            quiz_dict[Math.floor( Math.random() * 100 )]['en']
-            // デバッグ
+
+        correct_answer = quiz_dict[Math.floor( Math.random() * 100 )]
+
+        document.getElementById('quiz_en').innerHTML = correct_answer['en']
+
+        answer_display_list[0].innerHTML = quiz_dict[Math.floor( Math.random() * 100 )]['jp']
+        answer_display_list[1].innerHTML = quiz_dict[Math.floor( Math.random() * 100 )]['jp']
+        answer_display_list[2].innerHTML = quiz_dict[Math.floor( Math.random() * 100 )]['jp']
+        answer_display_list[3].innerHTML = quiz_dict[Math.floor( Math.random() * 100 )]['jp']
+
+        var correct_answer_index = Math.floor(Math.random() * 4);
+        answer_display_list[correct_answer_index].innerHTML = correct_answer['jp']
 
 }
 })();
