64abe5ea3222a30cb4ccb9eb3abe3a76f2822904
Yut40 <marshkip@gmail.com>
2021-08-07 13:18:12 +0900

    english quiz write testing

--- english_quiz_write/main.js
+++ english_quiz_write/main.js
@@ -10,6 +10,9 @@
 
         var quiz_dictionary = null
         var current_quiz = null
+        var is_in_quiz = false;
+        var quiz_correct_count = 0
+        var quiz_count = 0
 
         var loading_dictionary = function( url ){
             // 辞書ファイルの読み込み
@@ -22,19 +25,19 @@
             });
         }
         document.getElementById('difficulty_easy').onclick = function(){
-            loading_dictionary('https://roloa.github.io/english_quiz/easy.json')
+            loading_dictionary('https://roloa.github.io/english_quiz_write/easy.json')
             document.getElementById('difficulty_easy').style["background-color"] = '#FFC'
             document.getElementById('difficulty_normal').style["background-color"] = null
             document.getElementById('difficulty_hard').style["background-color"] = null
         }
         document.getElementById('difficulty_normal').onclick = function(){
-            loading_dictionary('https://roloa.github.io/english_quiz/normal.json')
+            loading_dictionary('https://roloa.github.io/english_quiz_write/normal.json')
             document.getElementById('difficulty_easy').style["background-color"] = null
             document.getElementById('difficulty_normal').style["background-color"] = '#FFC'
             document.getElementById('difficulty_hard').style["background-color"] = null
         }
         document.getElementById('difficulty_hard').onclick = function(){
-            loading_dictionary('https://roloa.github.io/english_quiz/hard.json')
+            loading_dictionary('https://roloa.github.io/english_quiz_write/hard.json')
             document.getElementById('difficulty_easy').style["background-color"] = null
             document.getElementById('difficulty_normal').style["background-color"] = null
             document.getElementById('difficulty_hard').style["background-color"] = '#FFC'
@@ -44,6 +47,10 @@
 
         // 出題ボタン
         document.getElementById('button_new_quiz').onclick = function(){
+
+            is_in_quiz = true
+            input_answer = document.getElementById('answer').value = ''
+
             current_quiz = quiz_dictionary[Math.floor( Math.random() * quiz_dictionary.length )]
 
             quiz_display_en.innerHTML = ""
@@ -62,14 +69,81 @@
 
         // 確定ボタン
         document.getElementById("button_confirm_answer").onclick = function(){
+
+
+            var is_correct_answer = false
+
+            var input_answer = document.getElementById('answer').value
+
             quiz_display_en.innerHTML = "";
 
+            // 正答を比較する
             for(var count = 0 ; count < current_quiz['en'].length ; count++ ){
-                quiz_display_en.innerHTML += "<button class='quiz_en'>" +
-                current_quiz['en'][count] +
-                "</button>"
+                if( current_quiz['en'].charAt( count ) == input_answer.charAt( count ) ){
+                    is_correct_answer = true
+                    quiz_display_en.innerHTML += "<button class='quiz_en_correct'>" +
+                    current_quiz['en'][count] +
+                    "</button>"
+                } else {
+                    quiz_display_en.innerHTML += "<button class='quiz_en_incorrect'>" +
+                    current_quiz['en'][count] +
+                    "</button>"
+                }
             }
 
+            // 別解の一覧を出す
+            quiz_display_description.innerHTML = "別解: "
+            if( current_quiz['synonyms'].length == 0 ){
+                quiz_display_description.innerHTML += "なし"
+            } else {
+
+                // 別解を探す
+                for(var count = 0 ; count < current_quiz['synonyms'].length && count < 10; count++ ){
+                    if( current_quiz['synonyms'][ count ] == input_answer ){
+                        // 別解正解
+                        is_correct_answer = true
+                        quiz_display_description.innerHTML += "<span class='synonyms_item synonyms_item_correct'>" +
+                        current_quiz['synonyms'][count] + ", " +
+                        "</span>"
+                        break;
+                    }
+                }
+
+
+                for(var count = 0 ; count < current_quiz['synonyms'].length && count < 10; count++ ){
+                    quiz_display_description.innerHTML += "<span class='synonyms_item'>" +
+                    current_quiz['synonyms'][count] + ", " +
+                    "</span>"
+                }
+                if( 10 < current_quiz['synonyms'].length ) {
+                    quiz_display_description.innerHTML += "... 他 " +
+                    (current_quiz['synonyms'].length - 10) +
+                    " ."
+                }
+            }
+
+            quiz_count += 1
+            if( is_in_quiz ) {
+                if( is_correct_answer ) {
+                    quiz_correct_count += 1
+                }
+                var correct_rate = Math.round((quiz_correct_count / quiz_count) * 100 )
+                document.getElementById('status_text').innerHTML = quiz_count + '問中' + quiz_correct_count + "問正解("+ correct_rate +"%)"
+            }
+
+            is_in_quiz = false
+
+        }
+
+        // エンターキー制御
+        document.onkeypress = function(){
+            if( window.event.keyCode == 13 ){
+                if( is_in_quiz ) {
+                    document.getElementById("button_confirm_answer").onclick()
+                } else {
+                    document.getElementById("button_new_quiz").onclick()
+                }
+            }
         }
     })();
 })();
