0ddca3df079b15a86bbd009c98b10636ac21f797
Yut40 <marshkip@gmail.com>
2021-10-11 14:31:45 +0900

    pond

--- time_rain/game.js
+++ time_rain/game.js
@@ -88,82 +88,26 @@ export class Game {
             this.add_sprite( new Sprites.Raindrop(), new_rain_x , new_rain_y )
         }
 
-        // 時計
+        // 時計のパラメータ計算
         let date = new Date()
 
-        this.canvas2d.save()
-        this.canvas2d.translate( this.canvas.width * 0.5 , this.canvas.height * 0.5 )
         let hour_rot = ((date.getHours()+(date.getMinutes()*0.0166)-3)/6) * Math.PI
-        this.canvas2d.rotate(hour_rot)
-        this.canvas2d.fillStyle = "rgb(255,255,255)"
         let hour_hand_length = this.shorter_width * 0.2
         let hour_hand_thick  = this.shorter_width * 0.04
 
-        this.canvas2d.translate(-hour_hand_length/10, -hour_hand_thick/2 )
-        this.canvas2d.fillRect(0, 0, hour_hand_length*1.1 , hour_hand_thick )
-        this.canvas2d.restore()
-
-        this.canvas2d.save()
-        this.canvas2d.translate( this.canvas.width * 0.5 , this.canvas.height * 0.5 )
         let minute_rot = ((date.getMinutes()+(date.getSeconds()*0.0166)-15)/30) * Math.PI
-        this.canvas2d.rotate(minute_rot)
-        this.canvas2d.fillStyle = "rgb(255,255,255)"
         let minute_hand_length = this.shorter_width * 0.4
         let minute_hand_thick  = this.shorter_width * 0.02
-        this.canvas2d.translate(-minute_hand_length/10, -minute_hand_thick/2 )
-        this.canvas2d.fillRect(0, 0, minute_hand_length*1.1 , minute_hand_thick )
-        this.canvas2d.restore()
 
-        this.canvas2d.save()
-        this.canvas2d.translate( this.canvas.width * 0.5 , this.canvas.height * 0.5 )
         let second_rot = ((date.getSeconds()+(date.getMilliseconds()*0.001)-15)/30) * Math.PI
-        this.canvas2d.rotate(second_rot)
-        this.canvas2d.fillStyle = "rgb(255,255,255)"
         let second_hand_length = this.shorter_width * 0.35
         let second_hand_thick  = this.shorter_width * 0.001
-        this.canvas2d.translate(-second_hand_length/10, -second_hand_thick/2 )
-        this.canvas2d.fillRect(0, 0, second_hand_length*1.1 , second_hand_thick )
-        this.canvas2d.restore()
 
-        // 風向
-        this.wind *= 0.9
-        this.wind += Math.random() * this.wind_changing
-        this.wind_changing *= 0.997
-        if( Math.random() < 0.01){
-            this.wind_changing += Math.random() * 0.2 - 0.1
+        if( false ){
+            // テストモード
+            hour_rot　 = second_rot * -2
+            minute_rot = second_rot * 5
         }
-
-        // 水たまり
-        // 数学がわからなくなったので断念
-        // // 秒針の先端座標
-        // let second_x = second_hand_length * Math.cos( second_rot )
-        // let second_y = second_hand_length * Math.sin( second_rot )
-        //
-        // // 長針の先端座標
-        // let minute_x = second_x
-        // let minute_y = second_y
-        // //this.add_sprite( new Sprites.Sparkle(), minute_x, minute_y )
-        //
-        // // 短針の先端座標
-        // let hour_x = hour_hand_length * Math.cos( hour_rot )
-        // let hour_y = hour_hand_length * Math.sin( hour_rot )
-        // //this.add_sprite( new Sprites.Sparkle(), hour_x, hour_y )
-        //
-        // if( 0 < minute_y && 0 < hour_y ){
-        //     // 両針の先が真ん中より上にある
-        //     let hour_actual_x = hour_x + this.canvas.width * 0.5
-        //     let hour_actual_y = hour_y + this.canvas.height * 0.5
-        //     let minute_actual_x = minute_x + this.canvas.width * 0.5
-        //     let minute_actual_y = minute_y + this.canvas.height * 0.5
-        //     if( hour_y < minute_y ){
-        //         // 短針のほうが上
-        //
-        //     } else {
-        //
-        //     }
-        //
-        //
-        // }
 
         // 各スプライトの処理と描画
         for(let i in this.sprite_list){
@@ -176,6 +120,156 @@ export class Game {
                 this.sprite_list[ i ].on_draw( this.canvas2d )
             }
         }
+
+        // 水たまり
+
+        // 秒針の先端座標
+        let second_x = second_hand_length * Math.cos( second_rot )
+        let second_y = second_hand_length * Math.sin( second_rot )
+
+        // 長針の先端座標
+        let minute_x = minute_hand_length * Math.cos( minute_rot )
+        let minute_y = minute_hand_length * Math.sin( minute_rot )
+        if( 0 < minute_x ){
+            // 針が右側の位置にあるなら、針の太さ分だけ左側に
+            minute_x += minute_hand_thick * Math.cos( minute_rot - Math.PI * 0.5 ) * 0.5
+            minute_y += minute_hand_thick * Math.sin( minute_rot - Math.PI * 0.5 ) * 0.5
+        } else {
+            minute_x += minute_hand_thick * Math.cos( minute_rot + Math.PI * 0.5 ) * 0.5
+            minute_y += minute_hand_thick * Math.sin( minute_rot + Math.PI * 0.5 ) * 0.5
+        }
+
+        // 短針の先端座標
+        let hour_x = hour_hand_length * Math.cos( hour_rot )
+        let hour_y = hour_hand_length * Math.sin( hour_rot )
+        if( 0 < hour_x ){
+            // 針が右側の位置にあるなら、針の太さ分だけ左側に
+            hour_x += hour_hand_thick * Math.cos( hour_rot - Math.PI * 0.5 ) * 0.5
+            hour_y += hour_hand_thick * Math.sin( hour_rot - Math.PI * 0.5 ) * 0.5
+        } else {
+            hour_x += hour_hand_thick * Math.cos( hour_rot + Math.PI * 0.5 ) * 0.5
+            hour_y += hour_hand_thick * Math.sin( hour_rot + Math.PI * 0.5 ) * 0.5
+        }
+        //this.add_sprite( new Sprites.Sparkle(), hour_x, hour_y )
+
+        if( minute_y　< 0 && hour_y < 0){
+            // 両針の先が真ん中より高い位置にある
+            let hour_actual_x = hour_x + this.canvas.width * 0.5
+            let hour_actual_y = hour_y + this.canvas.height * 0.5
+            let minute_actual_x = minute_x + this.canvas.width * 0.5
+            let minute_actual_y = minute_y + this.canvas.height * 0.5
+            if( hour_y < minute_y ){
+                // 短針のほうが上
+                // 交点のy座標と針先のy座標の割合を、針先のx座標に掛ける
+                let hour_cross_x = Math.abs(minute_y / hour_y) * hour_x + this.canvas.width * 0.5
+                this.canvas2d.strokeStyle = 'rgb(0,0,0)'
+                this.canvas2d.fillStyle = 'rgb(1,1,10)'
+                this.canvas2d.beginPath()
+                this.canvas2d.moveTo( minute_actual_x , minute_actual_y )
+                this.canvas2d.lineTo( hour_cross_x , minute_actual_y )
+                this.canvas2d.lineTo( this.canvas.width * 0.5 ,this.canvas.height * 0.5 )
+                this.canvas2d.fill()
+
+                this.canvas2d.fillStyle = 'rgba(0,0,0,0)'
+                this.canvas2d.strokeStyle = 'rgb(0,0,255)'
+                this.canvas2d.beginPath()
+                this.canvas2d.moveTo( minute_actual_x , minute_actual_y )
+                let start_x = minute_actual_x
+                let goal_x = hour_cross_x
+                if( goal_x <= start_x ){
+                    start_x = hour_cross_x
+                    goal_x = minute_actual_x
+                }
+                for( let ripple_x = start_x ; ripple_x < goal_x ; ripple_x += Math.random() * 10 ){
+                    if( Math.random() < 0.02) {
+                        this.canvas2d.lineTo( ripple_x , minute_actual_y + Math.random() * 10 - 5)
+                    } else {
+                        this.canvas2d.lineTo( ripple_x , minute_actual_y )
+                    }
+                }
+                this.canvas2d.lineTo( hour_cross_x , minute_actual_y )
+                this.canvas2d.stroke()
+
+                //長針の先から雨がこぼれる
+                // let new_drop = new Sprites.Raindrop()
+                // if( 0 < minute_x ) {
+                //     new_drop.dx = 2
+                // } else {
+                //     new_drop.dx = -2
+                // }
+                // this.add_sprite( new_drop, minute_actual_x, minute_actual_y)
+
+            } else {
+                let minute_cross_x = Math.abs(hour_y / minute_y) * minute_x + this.canvas.width * 0.5
+
+                this.canvas2d.strokeStyle = 'rgb(0,0,0)'
+                this.canvas2d.fillStyle = 'rgb(1,1,10)'
+                this.canvas2d.beginPath()
+                this.canvas2d.moveTo( hour_actual_x , hour_actual_y )
+                this.canvas2d.lineTo( minute_cross_x , hour_actual_y )
+                this.canvas2d.lineTo( this.canvas.width * 0.5 ,this.canvas.height * 0.5 )
+                this.canvas2d.fill()
+
+                this.canvas2d.fillStyle = 'rgba(0,0,0,0)'
+                this.canvas2d.strokeStyle = 'rgb(0,0,255)'
+                this.canvas2d.beginPath()
+                this.canvas2d.moveTo( hour_actual_x , hour_actual_y )
+                let start_x = hour_actual_x
+                let goal_x = minute_cross_x
+                if( goal_x <= start_x ){
+                    start_x = minute_cross_x
+                    goal_x = hour_actual_x
+                }
+                for( let ripple_x = start_x ; ripple_x < goal_x ; ripple_x += Math.random() * 10 ){
+                    if( Math.random() < 0.02) {
+                        this.canvas2d.lineTo( ripple_x , hour_actual_y + Math.random() * 10 - 5)
+                    } else {
+                        this.canvas2d.lineTo( ripple_x , hour_actual_y )
+                    }
+                }
+                this.canvas2d.lineTo( minute_cross_x , hour_actual_y )
+                this.canvas2d.stroke()
+
+
+            }
+        }
+
+        // 時計の描画
+        this.canvas2d.save()
+        this.canvas2d.translate( this.canvas.width * 0.5 , this.canvas.height * 0.5 )
+        this.canvas2d.rotate(hour_rot)
+        this.canvas2d.fillStyle = "rgb(255,255,255)"
+        this.canvas2d.translate(-hour_hand_length/10, -hour_hand_thick/2 )
+        this.canvas2d.fillRect(0, 0, hour_hand_length*1.1 , hour_hand_thick )
+        this.canvas2d.restore()
+
+        this.canvas2d.save()
+        this.canvas2d.translate( this.canvas.width * 0.5 , this.canvas.height * 0.5 )
+        this.canvas2d.rotate(minute_rot)
+        this.canvas2d.fillStyle = "rgb(255,255,255)"
+        this.canvas2d.translate(-minute_hand_length/10, -minute_hand_thick/2 )
+        this.canvas2d.fillRect(0, 0, minute_hand_length*1.1 , minute_hand_thick )
+        this.canvas2d.restore()
+
+        this.canvas2d.save()
+        this.canvas2d.translate( this.canvas.width * 0.5 , this.canvas.height * 0.5 )
+        this.canvas2d.rotate(second_rot)
+        this.canvas2d.fillStyle = "rgb(255,255,255)"
+        this.canvas2d.translate(-second_hand_length/10, -second_hand_thick/2 )
+        this.canvas2d.fillRect(0, 0, second_hand_length*1.1 , second_hand_thick )
+        this.canvas2d.restore()
+
+        // 風向
+        this.wind *= 0.9
+        this.wind += Math.random() * this.wind_changing
+        this.wind_changing *= 0.997
+        if( Math.random() < 0.01){
+            this.wind_changing += Math.random() * 0.2 - 0.1
+        }
+
+
+
+
 
     }
 }
