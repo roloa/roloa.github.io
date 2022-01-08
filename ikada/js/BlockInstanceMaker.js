cc15d78b08ce70a88f9eb29357fb74d4506b259b
Yut40 <marshkip@gmail.com>
2022-01-08 21:22:22 +0900

    image

--- ikada/js/BlockInstanceMaker.js
+++ ikada/js/BlockInstanceMaker.js
@@ -1,4 +1,5 @@
 
+import {ShipFarmWet} from './ship_block/ShipFarmWet.js'
 import {WaterPlace} from './ship_block/WaterPlace.js'
 import {ShipBlock} from './ship_block/ShipBlock.js'
 import {BotHome} from './ship_block/BotHome.js'
@@ -6,9 +7,11 @@ import {ShipFrame} from './ship_block/ShipFrame.js'
 import {DryLack} from './ship_block/DryLack.js'
 import {ShipFloor} from './ship_block/ShipFloor.js'
 import {ShipFarm} from './ship_block/ShipFarm.js'
+import {WeaponAirCannon} from './ship_block/WeaponAirCannon.js'
 import {FuelEngine} from './ship_block/FuelEngine.js'
 import {VictoryRocket} from './ship_block/VictoryRocket.js'
 import {DroneHome} from './ship_block/DroneHome.js'
+import {WaterPlace2} from './ship_block/WaterPlace2.js'
 import {FirePlace} from './ship_block/FirePlace.js'
 
 export class BlockInstanceMaker {
@@ -19,6 +22,8 @@ export class BlockInstanceMaker {
 
         if( block_data == null ){
             return null;
+        } else if( block_data.class_name == 'ShipFarmWet' ){
+            return new ShipFarmWet( this.game );
         } else if( block_data.class_name == 'WaterPlace' ){
             return new WaterPlace( this.game );
         } else if( block_data.class_name == 'ShipBlock' ){
@@ -33,12 +38,16 @@ export class BlockInstanceMaker {
             return new ShipFloor( this.game );
         } else if( block_data.class_name == 'ShipFarm' ){
             return new ShipFarm( this.game );
+        } else if( block_data.class_name == 'WeaponAirCannon' ){
+            return new WeaponAirCannon( this.game );
         } else if( block_data.class_name == 'FuelEngine' ){
             return new FuelEngine( this.game );
         } else if( block_data.class_name == 'VictoryRocket' ){
             return new VictoryRocket( this.game );
         } else if( block_data.class_name == 'DroneHome' ){
             return new DroneHome( this.game );
+        } else if( block_data.class_name == 'WaterPlace2' ){
+            return new WaterPlace2( this.game );
         } else if( block_data.class_name == 'FirePlace' ){
             return new FirePlace( this.game );
         }
