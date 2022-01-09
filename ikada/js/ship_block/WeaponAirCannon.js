5cc21477f57eb3daf1004f6ef82c92853b6c7d62
Yut40 <marshkip@gmail.com>
2022-01-09 19:42:25 +0900

    air cannon atk 100

--- ikada/js/ship_block/WeaponAirCannon.js
+++ ikada/js/ship_block/WeaponAirCannon.js
@@ -27,7 +27,7 @@ export class WeaponAirCannon extends ShipBlock {
     setup_gun_data(){
         this.gun_data = {};
         // 基礎攻撃力
-        this.gun_data.basic_power = 10;
+        this.gun_data.basic_power = 100;
         // クールタイム(50=1秒)
         this.gun_data.cool_time = 50
 
